2396eda985e6dcb5d5c4a300e78e71f64d7a9e20
Bil4l <hamdaoui21@gmail.com>
2024-08-06 22:36:49 +0200

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BFIYOTT4.mjs";import"./chunk-4QUJPJ43.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-6SGXNHRO.mjs";import"./chunk-UAH2HDEB.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
