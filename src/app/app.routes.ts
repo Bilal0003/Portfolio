c212cee46c4948aeb76c54ff970be03a521ea765
Bilal0003 <125665395+Bilal0003@users.noreply.github.com>
2024-08-07 15:01:45 +0000

    edited angular.json still cant deploy to gh pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { WorksComponent } from './works/works.component';
 import { SortingVisualiserComponent } from './works/sorting-visualiser/sorting-visualiser.component';
 import { Component } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const routes: Routes = [
     
-    { path:'home', component:HomeComponent, title: "Home - Bilal's Portfolio"},
-    { path:'works', component:WorksComponent , title: "Works - Bilal's Portfolio"},
-    { path: 'works/project/sorting-visualiser', component:SortingVisualiserComponent},
-    { path:'', redirectTo: '/home', pathMatch:'full' },
+    { path: 'home', component:HomeComponent, title: "Home - Bilal's Portfolio"},
+    { path: 'works', component:WorksComponent , title: "Works - Bilal's Portfolio"},
+    { path: 'works/sorting-visualiser', component:SortingVisualiserComponent},
+    { path: '', redirectTo: 'home', pathMatch:'full' },
 
 ];
