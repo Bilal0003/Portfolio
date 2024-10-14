ac3dd30dd22d42db355f2ba69332b1ad4df6fa95
Bil4l <hamdaoui21@gmail.com>
2024-10-14 20:10:49 +0200

    backup commit. setting up quotes api within home/quotes component

--- src/app/home/quotes/quotes.component.ts
+++ src/app/home/quotes/quotes.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuotesService } from './quotes.service';
 
 @Component({
   selector: 'app-quotes',
   standalone: true,
   imports: [],
   templateUrl: './quotes.component.html',
-  styleUrl: './quotes.component.css'
+  styleUrl: './quotes.component.css',
 })
 export class QuotesComponent {
+  constructor(private quotesSvc: QuotesService) {}
 
+  ngOnInit() {
+    let QuotesObservable = this.quotesSvc.getQuotes();
+
+    let QuotesData = QuotesObservable.subscribe((quote) => {
+      console.log(quote);
+    });
+
+    console.log('quotes component initialized.');
+  }
 }
